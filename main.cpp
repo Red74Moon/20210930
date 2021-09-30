be5b459d5c0a90a322bc14a472e211b827b854cc
Red74Moon <fedoormaster@gmail.com>
2021-09-30 22:05:11 +0900

    함수 분리

--- main.cpp
+++ main.cpp
@@ -3,39 +3,54 @@
 //- 난이도 업 버전 : 정수 A가 10을 입력 받으면 while문을 탈출하시오
 
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
+bool StatusCheck();
+void Input();
+int  Process(int a, int b);
+void Output();
+
+int a, b;
+bool Status = true;
 
 int main()
 {
-	int a, b;
-	bool Status = true;
-
-	system("cls");
-
 	while (Status)
 	{
-		cout << "두 수를 입력하세요, 첫번째 수가 10이면 마침니다." << endl;
-		cout << "첫번째수 : ";
-		cin >> a;
+		Input();
+		
+		Process(a,b);
 
-		if (a == 10)
-		{
-			Status = false;
-			break;
-		}
-
-		cout << "두번째수 : ";
-		cin >> b;
-
-		cout << a << " + " << b << " = " << a + b << endl << endl << endl;
-
-		if (a == 10)
-		{
-			Status = false;
-		}
-
+		Output();
 	}
 	return 0;
+}
+
+bool StatusCheck()
+{
+	if (a == 10)
+		Status = false;
+	return Status;
+}
+
+void Input()
+{
+	cout << "두 수를 입력하세요(정수), 첫번째 수가 10이면 마침니다." << endl;
+	cout << "첫번째수 : ";
+	cin >> a;
+
+	StatusCheck();
+
+	cout << "두번째수 : ";
+	cin >> b;
+}
+
+int Process(int a, int b)
+{
+	return a + b;
+}
+
+void Output()
+{
+	cout << a << " + " << b << " = " << Process(a, b) << endl << endl << endl;
 }
